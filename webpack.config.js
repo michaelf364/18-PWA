3b8ed0b36f760604a1cfa4e7ac8f5ffa886945bf
Michael Farris <farris.michael.j@gmail.com>
2020-08-04 18:04:05 -0700

    messing with webpack and service-worker settings

--- webpack.config.js
+++ webpack.config.js
@@ -9,33 +9,40 @@ const config = {
   mode: "development",
   plugins: [
     new WebpackPwaManifest({
-      // the name of the generated manifest file
-      filename: "manifest.json",
-
-      // we aren't using webpack to generate our html so we
-      // set inject to false
-      inject: false,
-
-      // set fingerprints to `false` to make the names of the generated
-      // files predictable making it easier to refer to them in our code
       fingerprints: false,
-
+      inject: false,
       name: "Budget Tracker",
+      // eslint-disable-next-line camelcase
       short_name: "Budget Tracker",
+      description: "An application for budget tracking",
+      // eslint-disable-next-line camelcase
+      background_color: "#01579b",
+      // eslint-disable-next-line camelcase
       theme_color: "#ffffff",
-      background_color: "#ffffff",
+      // eslint-disable-next-line camelcase
       start_url: "/",
-      display: "standalone",
-
       icons: [
         {
-          src: path.resolve(__dirname, "./public/icons/icon-512x512.png"),
-          // the plugin will generate an image for each size
-          // included in the size array
-          size: [72, 96, 128, 144, 152, 192, 384, 512],
+          src: path.resolve("public/icons/icon-512x512.png"),
+          sizes: [72, 96, 128, 144, 152, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
         },
       ],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
 module.exports = config;
